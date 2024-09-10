b9aaa37833a370af0c91cba1c78c06c0f5de1431
JOSE CARLOS BARRETO FILHO <85513751+JCARLOS3009@users.noreply.github.com>
2024-09-09 22:10:57 -0300

    Create database.sql

---
+++ database.sql
@@ -0,0 +1,29 @@
+CREATE TABLE Casa (
+    id_casa INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+    fase_da_construcao TEXT,
+    modelo_casa INTEGER
+);
+
+CREATE TABLE Materiais (
+    id_material INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+    nome TEXT,
+    quantidade INTEGER,
+    preco FLOAT,
+    id_casa INTEGER,
+    FOREIGN KEY (id_casa) REFERENCES Casa(id_casa)
+);
+
+CREATE TABLE Familia (
+    id_familia INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+    nome TEXT,
+    numero_membros INTEGER,
+    id_casa INTEGER,
+    FOREIGN KEY (id_casa) REFERENCES Casa(id_casa)
+);
+CREATE TABLE Voluntario (
+    user_id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+    nome TEXT,
+    email TEXT,
+    senha TEXT,
+    funcao TEXT CHECK(funcao IN ('lider', 'voluntario'))
+);
